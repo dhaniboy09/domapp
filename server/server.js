d15aec2fdd861cf8e3c7a41ffd3893e6f085f67e
Dhani <Dhani@Andelas-MacBook-Pro.local>
2017-07-11 18:21:38 +0100

    heroku push v10-1

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../webpack.config.dev.babel';
+// import webpackHotMiddleware from 'webpack-hot-middleware';
+// import webpackConfig from '../webpack.config.dev.babel';
 
 // const express = require('express');
 // const logger = require('morgan');
