ea16e81ec3e1e5c746d42fb1caa018885c39736a
Dhani <Dhani@Andelas-MacBook-Pro.local>
2017-07-11 20:12:46 +0100

    heroku push v21

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,12 +1,18 @@
 // This will be our application entry. We'll setup our server here.
 import http from 'http';
 import app from '../server';
+import server from '../app';
 
 // const http = require('http');
 // const app = require('../app'); // The express app we just created
-
-const port = parseInt(process.env.PORT, 10) || 8000;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port);
+if (process.env.NODE_ENV === 'production') {
+	const port = parseInt(process.env.PORT, 10) || 8000;
+	app.set('port', port);
+	const server = http.createServer(app);
+	server.listen(port);
+} else {
+	const port = parseInt(process.env.PORT, 10) || 8000;
+	app.set('port', port);
+	const server = http.createServer(server);
+	server.listen(port);
+}
